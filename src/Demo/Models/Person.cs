28a1a1d6183b66eba2f65eeee0944b8924e6db20
paulobusch <paulo202015@outlook.com.br>
2020-06-03 20:31:24 -0300

    Classe para validação

---
+++ src/Demo/Models/Person.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Demo.Models
+{
+    public class Person
+    {
+        public string FullName { get; private set; }
+        public int Age { get; private set; }
+        public string Email { get; private set; }
+        public DateTime BirthDate { get; private set; }
+        public bool HasDiscount { get; private set; }
+        public decimal Discount { get; private set; }
+
+        public Person() { }
+
+        public Person(
+            string fullName,
+            int age,
+            string email,
+            DateTime birthDate,
+            bool hasDiscount,
+            decimal discount
+        ) : this()
+        {
+            this.FullName = fullName;
+            this.Age = age;
+            this.Email = email;
+            this.BirthDate = birthDate;
+            this.HasDiscount = hasDiscount;
+            this.Discount = discount;
+        }
+    }
+}
