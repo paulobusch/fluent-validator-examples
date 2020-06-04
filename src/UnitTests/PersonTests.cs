c24e5e646d7df63f25f54c5223766ee226869fab
paulobusch <paulo202015@outlook.com.br>
2020-06-03 21:10:25 -0300

    Projeto de testes

---
+++ src/UnitTests/PersonTests.cs
@@ -0,0 +1,18 @@
+﻿using Demo.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTests
+{
+    public class PersonTests
+    {
+        public void Validator(
+            bool isValid,
+            string field,
+            Person person
+        ) {
+
+        }
+    }
+}
